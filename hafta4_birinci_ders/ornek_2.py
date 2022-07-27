14f7b608c5d9d55a1a9c0ed329a4325fdce8300b
TalhaO100050 <talhakilicarslan99@gmail.com>
2022-07-27 12:35:36 +0300

    Create ornek_2.py

---
+++ hafta4_birinci_ders/ornek_2.py
@@ -0,0 +1,8 @@
+kullanıcıAdı = input("Kullanıcı adı :")
+kullanıcıParola = input("Parola :")
+print(kullanıcıAdı == "Fatih" and kullanıcıParola == "Mayıs")
+
+print("İstanbulun Fethi 1453")
+tarih = input("Bu yıl :")
+tarih = int(tarih)
+print(tarih-1453)
