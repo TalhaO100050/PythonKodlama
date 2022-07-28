418a95523ae6ebf72fb0e2c110c76dc279b6ebd2
TalhaO100050 <talhakilicarslan99@gmail.com>
2022-07-28 10:20:37 +0300

    Create ornek_1.py

---
+++ hafta4_ikinci_ders/ornek_1.py
@@ -0,0 +1,6 @@
+sinavPuani = int(input("Sınav puanınızı giriniz (0 - 100)"))
+if sinavPuani >= 85:print("Pek iyi")
+elif sinavPuani >= 70:print("İyi")
+elif sinavPuani >= 55:print("Orta")
+elif sinavPuani >= 45:print("Geçer")
+else:print("Kaldı")
