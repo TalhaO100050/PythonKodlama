212418b5ba2bed98b03e1b6dc11952e78ffcd45f
TalhaO100050 <talhakilicarslan99@gmail.com>
2022-08-03 10:58:15 +0300

    Create ornek_1.py

---
+++ hafta5_birinci_ders/ornek_1.py
@@ -0,0 +1,40 @@
+liste = ["a","b"]
+liste[1] = 2
+print(liste)
+
+liste1 = ["a","b","c","d","e","f"]
+print(liste1)
+print(liste1[0])
+print(liste1[1])
+print(liste1[2])
+print(liste1[3])
+print(liste1[4])
+print(liste1[5])
+
+
+
+secilenRenk = "Gray"
+renkList = ["Red","Purple","Pink","Cyan","Black"]
+if secilenRenk in renkList:
+    print(secilenRenk,"listede var.")
+else:
+    print(secilenRenk,"listede yok.")
+
+
+
+print("Listede bir renk arayınız.")
+secilenRenk = input()
+renkList = ["Red","Purple","Pink","Cyan","Black"]
+if secilenRenk in renkList:
+    print(secilenRenk,"listede var.")
+else:
+    print(secilenRenk,"listede yok.")
+
+
+
+
+
+
+
+
+
