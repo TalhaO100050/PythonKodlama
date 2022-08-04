bc0542914dac2533aa326a2d8b12108c716b64a6
TalhaO100050 <talhakilicarslan99@gmail.com>
2022-08-04 10:53:54 +0300

    Create ornek_2_bolumSonuSorlular.py

---
+++ hafta5_ikinci_ders/ornek_2_bolumSonuSorlular.py
@@ -0,0 +1,63 @@
+#1. Soru
+
+harfler = ["a","e","i","o","i","u"]
+iHarfSayi = harfler.count("i")
+pHarfSayi = harfler.count("p")
+print("listede",iHarfSayi,"tane i,",pHarfSayi,"tane p vardır.")
+
+#-----------------------------------------------#
+print()
+print()
+print()
+#-----------------------------------------------#
+#2. Soru
+
+liste1,liste2,liste3,liste4 = ["merhaba", "ben", "Talha", ":)"]
+print(liste1)
+print(liste2)
+print(liste3)
+print(liste4)
+
+#-----------------------------------------------#
+print()
+print()
+print()
+#-----------------------------------------------#
+#3. Soru
+
+liste1 = [2,3,4]
+liste2 = [3,4,5]
+liste3 = [liste1[0] + liste1[1] + liste1[2] + liste2[0] + liste2[1] + liste2[2]]
+print(liste3)
+
+#-----------------------------------------------#
+print()
+print()
+print()
+#-----------------------------------------------#
+#4. Soru
+
+liste = [34,1,56,334,23,2,3,19]
+liste.sort()
+print(liste)
+liste.reverse()
+print(liste)
+
+#-----------------------------------------------#
+print()
+print()
+print()
+#-----------------------------------------------#
+#5. Soru
+
+print("Tebrikler")
+print("Türkiye")
+print("Merhaba")
+
+
+
+
+
+
+
+
