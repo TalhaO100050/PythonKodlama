6836faf26477613fa02b3ae06a49db04badded57
TalhaO100050 <talhakilicarslan99@gmail.com>
2022-08-24 12:59:41 +0300

    Create ornek_8_bolumSonuSorular.py

---
+++ hafta8_birinci_ders/ornek_8_bolumSonuSorular.py
@@ -0,0 +1,19 @@
+import turtle
+kalem = turtle.Turtle()
+for i in range(5):
+    kalem.forward(100)
+    kalem.right(144)
+kalem.clear()
+
+for i in range(20):
+    kalem.forward(10*i)
+    kalem.right(144)
+kalem.clear()
+
+for i in range(40):
+    kalem.forward(100)
+    kalem.left(123)
+kalem.clear()
+
+
+
